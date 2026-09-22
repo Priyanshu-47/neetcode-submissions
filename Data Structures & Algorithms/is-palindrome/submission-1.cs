15ed95df0741117930631a325c09b62747ff463f
Priyanshu-47 <85626783+Priyanshu-47@users.noreply.github.com>
2026-09-22 16:14:46 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cs
@@ -0,0 +1,34 @@
+public class Solution
+{
+    public bool IsPalindrome(string s)
+    {
+        int left = 0;
+        int right = s.Length - 1;
+
+        while (left < right)
+        {
+            // Skip non-alphanumeric characters from the left
+            while (left < right && !char.IsLetterOrDigit(s[left]))
+            {
+                left++;
+            }
+
+            // Skip non-alphanumeric characters from the right
+            while (left < right && !char.IsLetterOrDigit(s[right]))
+            {
+                right--;
+            }
+
+            // Compare case-insensitively
+            if (char.ToLower(s[left]) != char.ToLower(s[right]))
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
